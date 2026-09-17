25042b046e1bc705bed9286e7718188eeabfadaa
Caner Karaca <canerkaraca_23@hotmail.com>
2026-09-17 18:02:57 +0300

    feat(lights): add dynamic vehicle headlight pointlight colors

--- src/features/lights/lights.cpp
+++ src/features/lights/lights.cpp
@@ -7,6 +7,7 @@
 #include "ModelExtrasAPI.h"
 #include "utils/samp.h"
 #include <CCamera.h>
+#include <CPointLights.h>
 #include <eModelID.h>
 
 float gfGlobalCoronaSize = 0.3f;
@@ -116,6 +117,10 @@ void Lights::Init() {
 	patch::SetUChar(0x6E228B, 0);
 	patch::SetUChar(0x6E2532, 0);
 	patch::SetUChar(0x6E2627, 0);
+
+	// Disable native hardcoded white pointlights in CVehicle::DoVehicleLights
+	patch::Nop(0x6E27E6, 5);
+	patch::Nop(0x6E28E7, 5);
 
 	SAMP::PatchVehicleLights();
 
